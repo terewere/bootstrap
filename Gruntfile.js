a11fd7cf0bbb31a998a62afdded5f1679819f992
Shaibu Zachariyya <maddiiana@gmail.com>
2014-07-22 13:49:00 -0800

    working on grunt

---
+++ Gruntfile.js
@@ -0,0 +1,15 @@
+module.exports = function(grunt) {
+ grunt.loadNpmTasks('grunt-contrib-uglify');
+grunt.initConfig({
+  uglify: {
+    my_target: {
+      files: {
+        '_/js/script.js': ['_/components/js/script.js', 'src/input2.js']
+      }
+    }
+  }
+});
+
+
+
+  
